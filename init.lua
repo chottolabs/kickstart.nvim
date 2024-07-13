1b31403afad2baf25494f2a278d27a6d3b3763dd
chottolabs <171991982+chottolabs@users.noreply.github.com>
2024-07-13 00:35:16 +0000

    resolve which-key changes

--- init.lua
+++ init.lua
@@ -194,10 +194,11 @@ require('lazy').setup({
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
+      local wk = require 'which-key'
+      wk.setup()
 
       -- Document existing key chains
-      require('which-key').add {
+      wk.add {
         { '<leader>c', group = '[C]ode' },
         { '<leader>d', group = '[D]ocument' },
         { '<leader>r', group = '[R]ename' },
