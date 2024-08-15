b712dbdeb938fd23bf5154fd4137c2f10f9b7797
chottolabs <171991982+chottolabs@users.noreply.github.com>
2024-08-15 17:07:14 -0400

    some margin is nice

--- init.lua
+++ init.lua
@@ -882,9 +882,9 @@ require('lazy').setup({
           --  full:  full width of the window
           width = 'block',
           -- Amount of padding to add to the left of code blocks
-          left_pad = 0,
+          left_pad = 1,
           -- Amount of padding to add to the right of code blocks when width is 'block'
-          right_pad = 4,
+          right_pad = 2,
           -- Minimum width to use for code blocks when width is 'block'
           min_width = 0,
           -- Determins how the top / bottom of code block are rendered:
